0372d86b3efdd282b3a31668217e57a6f4ce6bee
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-02-24 16:57:14 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", (event) => {
+  event.preventDefault()
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let task = document.querySelector('#new-task-description')
+    buildToDo(task.value)
+    form.reset()
+  })
+})
+
+function buildToDo(toDo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${toDo} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
